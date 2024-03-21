099fa8dcf15688002956ce206c94c6cb2ba611f5
kwrona05 <kwrona05@gmail.com>
2024-03-21 03:37:36 +0100

    Program starts

--- Python playground.py
+++ Python playground.py
@@ -1,1 +1,10 @@
-print("Cześć!")
+def prawda(a, b):
+    a = input("Podaj wartość a: ")
+    b = input("Podaj wartość b: ")
+if a == b:
+    print("a = b")
+elif a > b:
+    print("a > b")
+else:
+    print("a < b")
+prawda()
